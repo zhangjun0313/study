b84327d0571dbb48528bd67a32a0d0e409f880ba
zhangjun0313 <1191788193@qq.com>
2017-09-05 22:39:44 +0800

    练习

---
+++ 8-07 拖拽图片 数据操作 完善/数据操作/data.js
@@ -0,0 +1,32 @@
+ var data = [
+    {
+        'id' : 1,
+        'username' : 'LEO',
+        'age' : 36,
+        'sex' : '男'
+    },
+    {
+        'id' : 2,
+        'username' : 'MOMO',
+        'age' : 16,
+        'sex' : '男'
+    },
+    {
+        'id' : 3,
+        'username' : 'DUODUO',
+        'age' : 29,
+        'sex' : '男'
+    },
+    {
+        'id' : 4,
+        'username' : '勇升',
+        'age' : 46,
+        'sex' : '男'
+    },
+    {
+        'id' : 5,
+        'username' : '小美',
+        'age' : 40,
+        'sex' : '女'
+    }
+];
