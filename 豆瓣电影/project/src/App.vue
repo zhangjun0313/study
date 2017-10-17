570963feeb870d6fa2a63e984e905562b9451f89
zhangjun0313 <1191788193@qq.com>
2017-10-17 23:12:43 +0800

    练习

---
+++ 豆瓣电影/project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Douban-header></Douban-header>
+    <router-view></router-view>
+    <DoubanFooter></DoubanFooter>
+  </div>
+</template>
+
+<script>
+import DoubanHeader from '@/components/header'
+import DoubanFooter from '@/components/footer'
+export default {
+  name: 'app',
+  components:{
+    DoubanHeader,
+    DoubanFooter
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ 豆瓣电影/project/src/views/content.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="content">
+		<Carousel></Carousel>
+		<!--loading-->
+		<!--<div class="fa fa-spinner fa-2x loading"></div>-->
+		<div class="nextFilm">
+			<div class="topDiv clearfix">
+				<span class="more">全部</span>
+				<a href="JavaScript:;">10月</a>
+				<a href="JavaScript:;">11月</a>
+				<a href="JavaScript:;">12月</a>
+				<span class="division-line">|</span>
+				<a href="JavaScript:;">时间</a>
+				<a href="JavaScript:;">热度</a>
+			</div>
+			<div class="bottomDiv" style="display: block;">10月17号，星期二</div>
+		</div>
+		<ul class="hotSubject">
+			<li>
+				<img src="../assets/img/mive-1.png" alt="羞羞的的铁拳" class="fl"/>
+				<div class="rightLi fl">
+					<h2>羞羞的铁拳</h2>
+					<p class="star clearfix">
+						<span class="fa fa-star fa-2x fl"></span>
+						<span class="fa fa-star fa-2x fl"></span>
+						<span class="fa fa-star fa-2x fl"></span>
+						<span class="fa fa-star-half-o fa-2x fl"></span>
+						<span class="fa fa-star-o fa-2x fl"></span>
+						<em class="fl">7.3</em>
+					</p>
+					<p class="director">导演：宋阳</p>
+					<p class="actor">主演：艾伦 / 马力 / 沈腾 / 田雨 / 宋阳 / 薛皓文 / 常远 / 黄才伦 / 王智</p>
+				</div>
+				<div class="ticket fr">
+					<p>16.1万人看过</p>
+					<botton>购票</botton>
+				</div>
+			</li>
+			<div class="bottomDiv">10月17号，星期二</div>
+			<li>
+				<img src="../assets/img/mive-1.png" alt="羞羞的的铁拳" class="fl"/>
+				<div class="rightLi fl">
+					<h2>羞羞的铁拳</h2>
+					<p class="star clearfix">
+						<span class="fa fa-star fa-2x fl"></span>
+						<span class="fa fa-star fa-2x fl"></span>
+						<span class="fa fa-star fa-2x fl"></span>
+						<span class="fa fa-star-half-o fa-2x fl"></span>
+						<span class="fa fa-star-o fa-2x fl"></span>
+						<em class="fl">7.3</em>
+					</p>
+					<p class="director">导演：宋阳</p>
+					<p class="actor">主演：艾伦 / 马力 / 沈腾 / 田雨 / 宋阳 / 薛皓文 / 常远 / 黄才伦 / 王智</p>
+				</div>
+				<div class="ticket fr">
+					<p>16.1万人看过</p>
+					<botton>购票</botton>
+				</div>
+			</li>
+			<div class="bottomDiv">10月17号，星期二</div>
+			<li>
+				<img src="../assets/img/mive-1.png" alt="羞羞的的铁拳" class="fl"/>
+				<div class="rightLi fl">
+					<h2>羞羞的铁拳</h2>
+					<p class="star clearfix">
+						<span class="fa fa-star fa-2x fl"></span>
+						<span class="fa fa-star fa-2x fl"></span>
+						<span class="fa fa-star fa-2x fl"></span>
+						<span class="fa fa-star-half-o fa-2x fl"></span>
+						<span class="fa fa-star-o fa-2x fl"></span>
+						<em class="fl">7.3</em>
+					</p>
+					<p class="director">导演：宋阳</p>
+					<p class="actor">主演：艾伦 / 马力 / 沈腾 / 田雨 / 宋阳 / 薛皓文 / 常远 / 黄才伦 / 王智</p>
+				</div>
+				<div class="ticket fr">
+					<p>16.1万人看过</p>
+					<botton>购票</botton>
+				</div>
+			</li>
+			<div class="bottomDiv">10月17号，星期二</div>
+			<li>
+				<img src="../assets/img/mive-1.png" alt="羞羞的的铁拳" class="fl"/>
+				<div class="rightLi fl">
+					<h2>羞羞的铁拳</h2>
+					<p class="star clearfix">
+						<span class="fa fa-star fa-2x fl"></span>
+						<span class="fa fa-star fa-2x fl"></span>
+						<span class="fa fa-star fa-2x fl"></span>
+						<span class="fa fa-star-half-o fa-2x fl"></span>
+						<span class="fa fa-star-o fa-2x fl"></span>
+						<em class="fl">7.3</em>
+					</p>
+					<p class="director">导演：宋阳</p>
+					<p class="actor">主演：艾伦 / 马力 / 沈腾 / 田雨 / 宋阳 / 薛皓文 / 常远 / 黄才伦 / 王智</p>
+				</div>
+				<div class="ticket fr">
+					<p>16.1万人看过</p>
+					<botton>购票</botton>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import Carousel from '@/components/carousel'
+export default {
+	components:{
+    Carousel
+  }
+}
+</script>
